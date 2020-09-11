1c39a81a76adac34fbbb390891c037b74cab7d8c
Justin Gent <justinbgent@gmail.com>
2020-09-11 12:00:21 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
